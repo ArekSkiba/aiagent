525b7a66d20006c8953c2c4bc6894ea95e4cf86b
ArekSkiba <arek.skiba77@gmail.com>
2025-06-10 20:56:27 +0200

    CH1 L5-L6 + CH2 L1-L2 messages --verbose, calculator base and files info function

---
+++ functions/get_files_info.py
@@ -0,0 +1,27 @@
+import os
+
+def get_files_info(working_directory, directory=None):
+    if directory == None:
+        directory = "."
+    if not os.path.isabs(directory):
+        directory = os.path.join(working_directory, directory)
+    if not os.path.abspath(directory).startswith(os.path.abspath(working_directory)):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
+    if not os.path.isdir(directory):
+        return f'Error: "{directory}" is not a directory'
+    
+    list_of_contents = []
+    try:
+        for item in os.listdir(directory):
+            item_path = os.path.join(directory, item)
+            if os.path.isfile(item_path):
+                list_of_contents.append(f"- {item}: file_size={os.path.getsize(item_path)}, is_dir=False")
+            else:
+                list_of_contents.append(f"- {item}: file_size={os.path.getsize(item_path)}, is_dir=True")
+            
+        return "\n".join(list_of_contents)
+    except Exception as e:
+        return f'Error: {str(e)}'
+
+    
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
+#komenda terminal do aktywacji venv: source venv/bin/activate
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -11,13 +14,22 @@ if len(sys.argv) < 2:
     print("No question was asked.")
     sys.exit(1)
 
-prompt = sys.argv[1]
+user_prompt = sys.argv[1]
 
-if prompt:
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
+
+if user_prompt:
     response = client.models.generate_content(
         model = 'gemini-2.0-flash-001', 
-        contents = prompt
+        contents=messages,
     )
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if '--verbose' in sys.argv:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
