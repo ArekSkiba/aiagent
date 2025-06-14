f8340dfd866f0253b9a67986e083cbcde782ac49
ArekSkiba <arek.skiba77@gmail.com>
2025-06-14 10:06:32 +0200

    CH3 L2-L3 function declarations (schema_get_files_info)

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 def get_file_content(working_directory, file_path):
     if file_path == None:
@@ -22,3 +23,18 @@ def get_file_content(working_directory, file_path):
         return file_content_string
     except Exception as e:
         return f'Error: {str(e)}'
+    
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Gets content of the given file.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the file to read from, relative to the working directory."
+            ),
+        },
+    ),
+)
